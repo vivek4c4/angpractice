f8dae44d315d2782d53467834c5e22918e26795d
vivek4c4 <vivek.ever@gmail.com>
2016-11-12 19:21:15 +0000

    practice 1

---
+++ app.js
@@ -0,0 +1,43 @@
+(function () {
+  angular.module("myFirstApp",[])
+  .controller("myFirstController",function($scope) {
+    $scope.name="";
+
+    $scope.check2much=function () {
+      var names=$scope.name;
+
+      var result=names.split(',');
+      var totallength=result.length;
+      var lenEmpty=0;
+      for (var i = 0; i < result.length; i++) {
+        if (result[i]!=null && result[i].length==1) {
+          lenEmpty++;
+
+        }
+      }
+      var correctLen=totallength-lenEmpty;
+      console.log("emptylen",lenEmpty);
+      console.log("corroect",correctLen);
+      console.log(result);
+      if (correctLen==1 && result=="") {
+        $scope.isempty="enter a value first";
+        $scope.color={"color":"red"};
+        $scope.bordercolor={"border-color":"red"};
+      }
+      else if (correctLen<=3) {
+        $scope.isempty="enjoy.!";
+        $scope.color={"color":"green"};
+        $scope.bordercolor={"border-color":"green"};
+      }
+      else if (correctLen>=4) {
+        $scope.isempty="Too much.!"
+        $scope.color={"color":"green"};
+        $scope.bordercolor={"border-color":"green"};
+      }
+      else{
+        $scope.isempty="";
+      }
+    };
+
+  })
+})();
